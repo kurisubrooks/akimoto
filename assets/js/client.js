79e3a6751d0aaf9329926b31d6666ec5a1b0704c
Kurisu Brooks <kurisubrooks@gmail.com>
2015-12-26 00:33:54 +1100

    Added Notifications & Login Error Handler

--- assets/js/client.js
+++ assets/js/client.js
@@ -3,6 +3,8 @@ $(function () {
     var authenticated = false;
     var username;
 
+    notify_getPerms();
+
     if (!authenticated) {
         $('.login').show();
         $('.overlay').show();
@@ -11,18 +13,49 @@ $(function () {
     function post_message(data) {
         console.log(data);
 
+        notify_trigger(data.username + ': ' + data.message);
+
         $('.client').append($(
             '<div class="chat_block" data-ts="' + data.ts + '">' +
-                '<img class="chat_img" src="' + data.icon + '" width="32px">' +
-                '<span id="chat_user">' + data.username + '</span>' +
-                '<span id="chat_ts">' + moment.unix(data.ts).format("hh:mma") + '</span>' +
-                '<span id="chat_msg">' + data.message + '</span>' +
+            '<img class="chat_img" src="' + data.icon + '" width="32px">' +
+            '<span id="chat_user">' + data.username + '</span>' +
+            '<span id="chat_ts">' + moment.unix(data.ts).format("hh:mma") + '</span>' +
+            '<span id="chat_msg">' + data.message + '</span>' +
             '</div>'
         ));
+    }
 
-        $(".client").animate({
-            scrollTop: $(document).height()
-        }, "slow");
+    function notify_getPerms() {
+        if (!('Notification' in window)) {
+            alert('Your browser doesn\'t support HTML5 Notifications. Consider upgrading your browser.');
+            return;
+        }
+
+        Notification.requestPermission(function (permission) {
+            if (Notification.permission == 'granted') {
+                var notification = new Notification('Akimoto', {
+                    body: 'Notifications Enabled.'
+                });
+
+                setTimeout(function () {
+                    notification.close();
+                }, 2500);
+            }
+        });
+    }
+
+    function notify_trigger(body) {
+        if (Notification.permission == 'granted') {
+            var notification = new Notification('Akimoto', {
+                body: body
+            });
+
+            setTimeout(function () {
+                notification.close();
+            }, 4500);
+        } else if (Notification.permission != 'denied') {
+            notify_getPerms();
+        }
     }
 
     socket.on('chat.post_message', function (data) {
@@ -35,27 +68,33 @@ $(function () {
     });
 
     $('#login_form').submit(function () {
-        socket.emit('auth.user_auth', {
-            "username": $('#auth_input').val()
-        });
+        if (String($('#auth_input').val()).length >= 16) {
+            $('#login_error').show();
+            $('#login_error').text('Error! This username is too long.');
+            console.log('Error: Username too long.');
+            return false;
+        } else {
+            socket.emit('auth.user_auth', {
+                "username": $('#auth_input').val()
+            });
 
-        username = $('#auth_input').val();
-        $('#auth_input').val('');
+            username = $('#auth_input').val();
+            $('#auth_input').val('');
 
-        socket.emit('chat.post_message', {
-            "username": username,
-            "icon": "https://avatars.slack-edge.com/2015-12-22/17173671875_21dcf6ae09fd3b9f261c_48.png",
-            "message": "<i>Joined</i>"
-        });
+            socket.emit('chat.post_message', {
+                "username": username,
+                "message": username + " joined!"
+            });
 
-        $('.login').hide();
-        $('.login').attr("disabled", true);
-        $('.overlay').fadeOut(350);
-        $('#chat_input').focus();
-        $('#chat_input').attr("autofocus");
+            $('.login').hide();
+            $('.login').attr("disabled", true);
+            $('.overlay').fadeOut(350);
+            $('#chat_input').focus();
+            $('#chat_input').attr("autofocus");
 
-        authenticated = true;
-        return false;
+            authenticated = true;
+            return false;
+        }
     });
 
     $('#chat_form').submit(function () {
@@ -71,7 +110,6 @@ $(function () {
             post_message({
                 "ts": moment().format('X'),
                 "username": "Akimoto",
-                "icon": "https://avatars.slack-edge.com/2015-12-22/17173671875_21dcf6ae09fd3b9f261c_48.png",
                 "message": "You need to log in before you can send a message!"
             });
 
