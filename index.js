6410294432ac22defcc6bb633a80d35914625ab0
Kurisu Brooks <kurisubrooks@gmail.com>
2015-12-28 00:02:15 +1100

    Check User against DB Authentication

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const express = require('express'),
 const core = require('./modules/core'),
       logger = require('crimson'),
       moment = require('moment'),
-      _ = require('lodash');
+      _ = require('lodash')
+      fs = require('fs');
       
 var users = {};
+var db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+var db_values = Object.keys(db.users);
 
 /*
+// Cause i've got a
 // Blank
 // Space
+// Baby,
 
 // And I'll
 // Write Your
@@ -30,17 +35,19 @@ function timestamp() {
     return moment().format('X');
 }
 
-/*
-// Socket
-*/
-
 function socket_error(api, error) {
     io.emit(api, {
         "ok": false,
         "reason": error,
-        "ts": timestamp();
+        "ts": timestamp()
     });
+    
+    return;
 }
+
+/*
+// Socket
+*/
 
 io.on('connection', function (socket) {
     //console.log(io.sockets.connected);
@@ -51,15 +58,12 @@ io.on('connection', function (socket) {
         logger.error('Client disconnected.');
     });
 
-    socket.on('auth.user_auth', function (data) {
-        if ((data.username).length >= 16) socket_error('auth.user_auth', 'Username must be less than 16 characters');
-        else if (_.where(io.sockets.connected, {username: data.username}, "id").length > 0) socket_error('auth.user_auth', 'User is already connected');
-        else {
-            // Authentication Successful
+    socket.on('auth.user', function (data) {
+        if (db_values.indexOf((data.username).toLowerCase()) >= 0) {            
             socket.username = data.username;
             users[socket.id] = data.username;
 
-            io.emit('auth.user_auth', {
+            io.emit('auth.user', {
                 "ok": true,
                 "ts": timestamp(),
                 "username": socket.username
@@ -67,10 +71,37 @@ io.on('connection', function (socket) {
 
             logger.success(socket.username + ' authenticated successfully.');
         }
+
+        else if (db_values.indexOf(data.username) == -1) {
+            socket_error('auth.user', 'User does not exist.');
+        }
+        
+        /*
+        else if ((data.username).length >= 16) {
+            socket_error('auth.user', 'Username must be less than 16 characters');
+        }
+        
+        else if (_.where(io.sockets.connected, {username: data.username}, 'id').length > 0) {
+            socket_error('auth.user', 'User is already connected');
+        }
+        
+        else {
+            socket.username = data.username;
+            users[socket.id] = data.username;
+
+            io.emit('auth.user', {
+                "ok": true,
+                "ts": timestamp(),
+                "username": socket.username
+            });
+
+            logger.success(socket.username + ' authenticated successfully.');
+        }
+        */
     });
 
-    socket.on('chat.post_message', function (data) {
-        io.emit('chat.post_message', {
+    socket.on('chat.post', function (data) {
+        io.emit('chat.post', {
             "ok": true,
             "ts": timestamp(),
             "username": socket.username,
