7dfbf5242921169a52f44ea9169e44ddec034d94
Kurisu Brooks <kurisubrooks@gmail.com>
2015-12-30 00:15:51 +1100

    Fixed Formatting

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     var socket = io.connect();
 
     var username;
@@ -6,7 +6,7 @@ $(function() {
     var authenticated = false;
     var connected = false;
     var typing = false;
-    
+
     var $status = $('#status');
     var $client = $('.client');
     var $login = $('.login');
@@ -16,49 +16,49 @@ $(function() {
     var $chatform = $('#chat_form');
     var $inputuser = $('#input_username');
     var $inputchat = $('#input_chatmsg');
-   
+
     // Open Login Window if not authenticated
     if (!authenticated) $login.show();
-    
+
     // Client Status
-    socket.on('connect', function() {$status.css('color', '#4ecc71');});
-    socket.on('reconnect', function() {$status.css('color', '#4ecc71');});
-	socket.on('timeout', function() {$status.css('color', '#e65757');});
-    socket.on('connect_timeout', function() {$status.css('color', '#e65757');});
-    socket.on('error', function() {$status.css('color', '#e65757');});
-    socket.on('disconnect', function() {$status.css('color', '#e65757');});
-    socket.on('reconnect_error', function() {$status.css('color', '#e65757');});
-    socket.on('reconnect_failed', function() {$status.css('color', '#e65757');});
-    
-    $status.click(function(){
+    socket.on('connect', function () {$status.css('color', '#4ecc71');});
+    socket.on('reconnect', function () {$status.css('color', '#4ecc71');});
+    socket.on('timeout', function () {$status.css('color', '#e65757');});
+    socket.on('connect_timeout', function () {$status.css('color', '#e65757');});
+    socket.on('error', function () {$status.css('color', '#e65757');});
+    socket.on('disconnect', function () {$status.css('color', '#e65757');});
+    socket.on('reconnect_error', function () {$status.css('color', '#e65757');});
+    socket.on('reconnect_failed', function () {$status.css('color', '#e65757');});
+
+    $status.click(function () {
         socket.disconnect();
         location.reload();
     });
-    
+
     function time() {
         return moment().format('X');
     }
-    
+
     function post(data) {
         console.log(data);
-        
+
         $client.append($(
             '<div class="chat_block" data-ts="' + data.ts + '">' +
-                //'<img class="chat_img" src="' + data.icon + '" width="32px">' +
-                '<span id="chat_user">' + data.username + '</span>' +
-                '<span id="chat_ts">' + moment.unix(data.ts).format("hh:mma") + '</span>' +
-                '<span id="chat_msg">' + data.message + '</span>' +
+            //'<img class="chat_img" src="' + data.icon + '" width="32px">' +
+            '<span id="chat_user">' + data.username + '</span>' +
+            '<span id="chat_ts">' + moment.unix(data.ts).format("hh:mma") + '</span>' +
+            '<span id="chat_msg">' + data.message + '</span>' +
             '</div>'
         ));
     }
 
-    socket.on('chat.post', function(data) {
+    socket.on('chat.post', function (data) {
         post(data);
     });
 
-    socket.on('user.join', function(data) {
+    socket.on('user.join', function (data) {
         console.log(data);
-        
+
         post({
             "ok": true,
             "ts": time(),
@@ -66,10 +66,10 @@ $(function() {
             "message": data.username + ' joined!'
         });
     });
-    
-    socket.on('user.auth', function(data) {
+
+    socket.on('user.auth', function (data) {
         console.log(data);
-        
+
         if (data.ok) {
             connected = true;
             username = $inputuser.val();
@@ -81,15 +81,13 @@ $(function() {
             $login.attr('disabled');
             $inputchat.focus();
             return false;
-        }
-        
-        else {
+        } else {
             $loginerr.show();
             $loginerr.text(data.message);
             $inputuser.css('border-color', '#e65757');
         }
     });
-    
+
     /*socket.on('user.quit', function(data) {
         console.log(data);
         
@@ -101,48 +99,42 @@ $(function() {
         });
     });*/
 
-    $loginform.submit(function() {
+    $loginform.submit(function () {
         if ($inputuser.val() === '') {
             $inputuser.css('border-color', '#e65757');
             return false;
-        }
-        
-        else {
+        } else {
             socket.emit('user.auth', {
                 "ok": true,
                 "ts": time(),
                 "username": $inputuser.val()
             });
-            
+
             return false;
         }
     });
-    
-    $chatform.submit(function() {
+
+    $chatform.submit(function () {
         if ($inputchat.val() === '') {
             $inputchat.css('border-color', '#e65757');
             return false;
-        }
-        
-        else if (username === undefined) {
+        } else if (username === undefined) {
             post({
                 "ok": false,
                 "ts": time(),
                 "username": notice_user,
                 "message": "You need to log in before you can send a message!"
             });
-            
+
             return false;
-        }
-        
-        else if (connected) {
+        } else if (connected) {
             socket.emit('chat.post', {
                 "ok": true,
                 "ts": time(),
                 "username": username,
                 "message": $inputchat.val()
             });
-            
+
             $inputchat.css('border-color', '#ddd');
             $inputchat.val('');
             return false;
--- index.js
+++ index.js
@@ -1,94 +1,95 @@
 var express = require('express'),
-	app = express(),
-	http = require('http').Server(app),
-	io = require('socket.io')(http),
-	crimson = require('crimson'),
-	moment = require('moment');
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    crimson = require('crimson'),
+    moment = require('moment');
 
 var port = 8080;
 app.use('/assets', express.static(__dirname + '/assets'));
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function() {
-	console.log('Listening on 127.0.0.1:%d', port);
+http.listen(port, function () {
+    console.log('Listening on 127.0.0.1:' + port);
 });
 
 /*
-//
+// Blank Space
+// ..aaaaand sued
 */
 
 var online_users = 0;
 
 function time() {
-	return moment().format('X');
+    return moment().format('X');
 }
 
-io.on('connection', function(socket) {
-	var user_authed = false;
+io.on('connection', function (socket) {
+    var user_authed = false;
 
-	socket.on('user.auth', function(data) {
-		if (user_authed) return;
+    socket.on('user.auth', function (data) {
+        if (user_authed) return;
 
-		socket.username = data.username;
-		user_authed = true;
-		++online_users;
-        
+        socket.username = data.username;
+        user_authed = true;
+        ++online_users;
+
         crimson.success(socket.username + ' joined.');
 
-		io.emit('user.count', {
-			"ok": true,
-			"ts": time(),
-			"online": online_users
-		});
-        
+        io.emit('user.count', {
+            "ok": true,
+            "ts": time(),
+            "online": online_users
+        });
+
         socket.emit('user.auth', {
             "ok": true,
             "ts": time(),
             "username": socket.username
         })
 
-		io.emit('user.join', {
-			"ok": true,
-			"ts": time(),
-			"username": socket.username,
-			"online": online_users
-		});
-	});
+        io.emit('user.join', {
+            "ok": true,
+            "ts": time(),
+            "username": socket.username,
+            "online": online_users
+        });
+    });
 
-	socket.on('chat.post', function(data) {
+    socket.on('chat.post', function (data) {
         crimson.info(socket.username + ': ' + data.message);
-        
-		io.emit('chat.post', {
-			"ok": true,
-			"ts": time(),
-			"username": socket.username,
-			"message": data.message
-		});
-	});
-    
-	socket.on('disconnect', function() {
-		if (user_authed) {
-            crimson.error(socket.username + ' left.');
-			--online_users;
 
-			io.emit('user.quit', {
-				"ok": true,
-				"ts": time(),
-				"username": socket.username,
-				"online": online_users
-			});
-            
+        io.emit('chat.post', {
+            "ok": true,
+            "ts": time(),
+            "username": socket.username,
+            "message": data.message
+        });
+    });
+
+    socket.on('disconnect', function () {
+        if (user_authed) {
+            crimson.error(socket.username + ' left.');
+            --online_users;
+
+            io.emit('user.quit', {
+                "ok": true,
+                "ts": time(),
+                "username": socket.username,
+                "online": online_users
+            });
+
             io.emit('chat.post', {
                 "ok": true,
                 "ts": time(),
                 "username": socket.username,
                 "message": socket.username + ' left.'
             });
-		}
-	});
-    
+        }
+    });
+
     /*
 	socket.on('chat.typing', function() {
 		socket.emit('user.typing', {
