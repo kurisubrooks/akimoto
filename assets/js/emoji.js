bc180acd6df4ee10d451492fd61852fa110a28bf
Kurisu Brooks <kurisubrooks@gmail.com>
2015-12-31 00:57:43 +1100

    Fixed Whitespace

--- assets/js/emoji.js
+++ assets/js/emoji.js
@@ -1,15 +1,15 @@
-function parse_emoji(emoloc) {
-  $.getJSON('./assets/js/emoji.json', function(data) {
-    var sheet_max = 40;
-    $.each(data, function(i) {
-      $(emoloc).text(function() {
-        var message = $(this).html();
-        var sheet_xp = (data[i].sheet_x / sheet_max) * 100;
-        var sheet_yp = (data[i].sheet_y / sheet_max) * 100;
-        var short_name = ':' + data[i].short_name + ':';
-        var out_html = '<span class="emoji-sizer"><span class="emoji" style="background-position:' + sheet_xp +'% ' + sheet_yp + '%;">' + short_name + '</span></span>';
-        $(this).html(message.replace(short_name, out_html));
-      });
+function emojify(input) {
+    $.getJSON('./assets/js/emoji.json', function(data) {
+        var sheet_max = 40;
+        $.each(data, function(i) {
+            $(input).text(function() {
+                var message = $(this).html();
+                var sheet_xx = (data[i].sheet_x / sheet_max) * 100;
+                var sheet_yy = (data[i].sheet_y / sheet_max) * 100;
+                var short_name = ':' + data[i].short_name + ':';
+                var output = '<span class="emoji-sizer"><span class="emoji" style="background-position:' + sheet_xx + '% ' + sheet_yy + '%;">' + short_name + '</span></span>';
+                $(this).html(message.replace(short_name, output));
+            });
+        });
     });
-  });
 }
