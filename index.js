09ec1dd4be79c7902c6e33ff11f05e8cf45f24e8
Kurisu Brooks <kurisubrooks@gmail.com>
2016-02-06 14:48:11 +1100

    Start Login Logic

--- index.js
+++ index.js
@@ -1,49 +1,82 @@
-var express = require('express'),
-    app = express(),
-    http = require('http').Server(app),
-    io = require('socket.io')(http),
-    //mongo = require('mongodb').MongoClient,
-    crimson = require('crimson'),
-    moment = require('moment');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
-var port = 8080;
-app.use('/assets', express.static(__dirname + '/assets'));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+const crimson = require('crimson');
+const moment = require('moment');
+const crypto = require('crypto');
+const uuid = require('node-uuid');
 
-http.listen(port, function () {
-    crimson.success('Listening on 127.0.0.1:' + port);
-});
+const keychain = require('./keychain');
+const auth = require('./auth');
+const port = 8080;
 
-/*
-// Blank Space
-// ..aaaaand sued
-*/
-
-var online_users = 0;
-var commands = ['help', 'me', 'away'];
+app.use('/assets', express.static(__dirname + '/public/assets'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret: keychain.session}));
 
 function time() {
     return moment().format('X');
 }
 
-io.on('connection', function (socket) {
+app.get('/',         (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/login',    (req, res) => res.sendFile(__dirname + '/public/login.html'));
+app.get('/register', (req, res) => res.sendFile(__dirname + '/public/register.html'));
+
+app.post('/api', (req, res) => {
+    var post = req.body;
+
+    if (post.type == 'login') {
+        
+    }
+    
+    if (post.type == 'register') {
+        auth.new_user(post);
+    }
+
+    console.log(type);
+    console.log(post);
+});
+
+http.listen(port, () => {
+    crimson.success('Listening on 127.0.0.1:' + port);
+});
+
+var online_count = 0;
+var online_users = [];
+
+io.on('connection', (socket) => {
     var user_authed = false;
 
-    socket.on('user.auth', function (data) {
+    socket.on('user.auth', (data) => {
         if (user_authed) return;
+        console.log(data);
 
+        auth.login(data, function(response) {
+            if (response.ok) {
+                console.log('response == ok');
+            }
+            
+            else {
+                console.log('response == not ok');
+            }
+        });
+
+        /*
         socket.username = data.username;
         user_authed = true;
-        ++online_users;
+        ++online_count;
 
         crimson.success(socket.username + ' joined.');
 
         io.emit('user.count', {
             "ok": true,
             "ts": time(),
-            "online": online_users
+            "online": online_count
         });
 
         socket.emit('user.auth', {
@@ -56,25 +89,33 @@ io.on('connection', function (socket) {
             "ok": true,
             "ts": time(),
             "username": socket.username,
-            "online": online_users
+            "online": online_count
         });
+        */
     });
 
-    socket.on('chat.post', function (data) {
+    socket.on('chat.post', (data) => {
         crimson.info(socket.username + ': ' + data.message);
 
-		if (data.message.startsWith('/')) {
-			crimson.debug('yay, a command :D');
+        io.emit('chat.post', {
+            "ok": true,
+            "ts": time(),
+            "username": socket.username,
+            "message": data.message
+        });
+
+        /*if (data.message.startsWith('/')) {
+        crimson.debug('yay, a command :D');
             var text = data.message;
                 text.slice(0, 1);
                 text.split(' ');
                 console.log(text);
-            
+
             if (text.length > 1 && commands.indexOf(text[0]) >= 0) {
                 crimson.debug('k great, the command exists');
                 console.log(text);
             }
-            
+
             else {
                 socket.emit('chat.post', {
                     "ok": false,
@@ -84,28 +125,19 @@ io.on('connection', function (socket) {
                     "message": text
                 });
             }
-		}
-        
-        else {
-            io.emit('chat.post', {
-                "ok": true,
-                "ts": time(),
-                "username": socket.username,
-                "message": data.message
-            });
-        }
+        }*/
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', () => {
         if (user_authed) {
             crimson.error(socket.username + ' left.');
-            --online_users;
+            --online_count;
 
             io.emit('user.quit', {
                 "ok": true,
                 "ts": time(),
                 "username": socket.username,
-                "online": online_users
+                "online": online_count
             });
 
             io.emit('chat.post', {
@@ -120,18 +152,18 @@ io.on('connection', function (socket) {
     /*
     socket.on('chat.typing', function() {
         socket.emit('user.typing', {
-    	    "ok": true,
-    	    "ts": time(),
-    	    "username": socket.username
+            "ok": true,
+            "ts": time(),
+            "username": socket.username
         });
-	});
+    });
 
-	socket.on('chat.typing.stop', function() {
+    socket.on('chat.typing.stop', function() {
         socket.emit('user.typing.stop', {
-    	    "ok": true,
-    	    "ts": time(),
-    	    "username": socket.username
+            "ok": true,
+            "ts": time(),
+            "username": socket.username
         });
-	});
+    });
     */
 });
