cdda80043b29bf4cc5f3e2290065cf4c477a87d7
Kurisu Brooks <kurisubrooks@gmail.com>
2016-02-06 20:33:18 +1100

    Added Error Checking for Login Page

---
+++ public/assets/js/login.js
@@ -0,0 +1,16 @@
+$(function() {
+    function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1));
+        var sURLVariables = sPageURL.split('&'), sParameterName, i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+    
+    var error = getUrlParameter('error');
+    if (error) {
+        $('#login-error').show();
+        $('#login-error').html('<b>Error: </b>' + error);
+    }
+});
